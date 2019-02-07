9df6931cdebf6adb1a261a1e10ba095473dd1877
daichi.tomonaga <jpx3033@linecorp.com>
2019-02-07 13:33:58 +0900

    相棒検索をクラスコンポーネント化

--- src/js/SearchPartner.js
+++ src/js/SearchPartner.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchPartner = (props) => {
+class SearchPartner extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
 
-  return(
+  render() {
     
-    <section className="searchPartner">
-      <div className="inputArea">
-      <h2>3. あなたの相棒ポケモン検索</h2>
-        <p className="inputAreaText">※名前はひらがなで</p>
-        <input 
-          id="inputName"
-          className="inputName"
-          type="text"
-          placeholder="ひらがなで名前を入力しよう"
-          />
-        <button onClick={e => props.decidePartner(e)}>決定</button>
-      </div>
-      <div className="outputArea">
-        <dl>
-          <dt>図鑑番号</dt>
-          <dd>{props.normalArray[Number(props.resultNo) - 1].no}</dd>
-          <dt>名前</dt>
-          <dd>{props.normalArray[Number(props.resultNo) - 1].name}</dd>
-          <dt>タイプ</dt>
-          <dd>
-            <span>{props.normalArray[Number(props.resultNo) - 1].types[0]}</span>
-            <span>{props.normalArray[Number(props.resultNo) - 1].types[1]}</span>
-          </dd>
-        </dl>
-        <div className={"img imgNo" + props.resultNo}></div>
-      </div>
-    </section>
+    return(
+      
+      <section className="searchPartner">
+        <div className="inputArea">
+        <h2>3. あなたの相棒ポケモン検索</h2>
+          <p className="inputAreaText">※名前はひらがなで</p>
+          <input 
+            id="inputName"
+            className="inputName"
+            type="text"
+            placeholder="ひらがなで名前を入力しよう"
+            />
+          <button onClick={e => this.props.decidePartner(e)}>決定</button>
+        </div>
+        <div className="outputArea">
+          <dl>
+            <dt>図鑑番号</dt>
+            <dd>{this.props.normalArray[Number(this.props.resultNo) - 1].no}</dd>
+            <dt>名前</dt>
+            <dd>{this.props.normalArray[Number(this.props.resultNo) - 1].name}</dd>
+            <dt>タイプ</dt>
+            <dd>
+              <span>{this.props.normalArray[Number(this.props.resultNo) - 1].types[0]}</span>
+              <span>{this.props.normalArray[Number(this.props.resultNo) - 1].types[1]}</span>
+            </dd>
+          </dl>
+          <div className={"img imgNo" + this.props.resultNo}></div>
+        </div>
+      </section>
 
   )
+
+  }
 }
 
 export default SearchPartner;
